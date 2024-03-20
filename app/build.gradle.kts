db1b811adb3f4eb817d8f97909175ae0ebef7236
Michael Gneuschev <michael.gneuschev@ausy-technologies.de>
2024-03-21 00:25:11 +0100

    Adds coil and displays image from url.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,8 +73,9 @@ dependencies {
     // Dagger Hilt
     implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-android-compiler:2.48")
-    //implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
-    //kapt("androidx.hilt:hilt-compiler:1.0.0")
+
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/example/starwarschallenge/presentation/character_details/components/CharacterDetailsScreen.kt
+++ app/src/main/java/com/example/starwarschallenge/presentation/character_details/components/CharacterDetailsScreen.kt
@@ -3,11 +3,19 @@ package com.example.starwarschallenge.presentation.character_details.components
 import CustomDeserializer
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.example.starwarschallenge.domain.model.Character
 import com.example.starwarschallenge.presentation.character_details.CharacterDetailsViewModel
 import com.google.gson.GsonBuilder
@@ -20,16 +28,32 @@ fun CharacterDetailsScreen(
 ) {
     val state = viewModel.state.value
     val characterDetails = deserializeCharacter(character)
+    val characterName = characterDetails.name
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(5.dp)
     ) {
-        Text(text = characterDetails.name)
+        Text(
+            text = characterName,
+            modifier = Modifier
+                .padding(20.dp)
+                .fillMaxWidth(),
+            style = TextStyle(
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            textAlign = TextAlign.Center
+        )
         Text(text = characterDetails.height.toString())
         Text(text = characterDetails.mass.toString())
         Text(text = characterDetails.gender)
         Text(text = formatList(characterDetails.homeworld))
         Text(text = characterDetails.wiki.toString())
-        Text(text = characterDetails.image.toString())
+        AsyncImage(
+            model = characterDetails.image.toString(),
+            contentDescription = "Portrait of $characterName",
+        )
         Text(text = characterDetails.born.toString())
         // Don't show in case of null
         characterDetails.bornLocation?.let { Text(text = it) }
